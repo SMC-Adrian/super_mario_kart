8d68855e1f961931f76fc3baa90ae3fdbcddecb4
Adrian Bleeker <204091@stmichaelcollege.nl>
2025-01-22 11:40:05 +0100

    main page

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+import pygame as pg
+import sys
 
+# initialiseer pygame
+pg.init()
+
+# maak scherm en geef titel
+SCREENWIDTH, SCREENHEIGHT = 1920, 1080
+screen = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pg.display.set_caption("Super Mario Kart - PO Informatica")
+
+# maak variabelen voor kleuren
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 175, 0)
+
+# laad afbeeldingen
+mario = pg.image.load(r"img/loadingscreen.webp.png")
+
+
+# beginscherm loop
+beginscherm = True
+while beginscherm:
+    pass
