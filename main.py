af17de99a4ab3c44181b3d45208406d3c3da3032
Adrian Bleeker <204091@stmichaelcollege.nl>
2025-01-23 11:29:39 +0100

    replace pg to pygame

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import pygame as pg
+import pygame 
 import sys
+import banen
 
 # initialiseer pygame
-pg.init()
+pygame.init()
 
 # maak scherm en geef titel
 SCREENWIDTH, SCREENHEIGHT = 1280, 720
-screen = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
-pg.display.set_caption("Super Mario Kart - PO Informatica")
+screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pygame.display.set_caption("Super Mario Kart - PO Informatica")
 
 # maak variabelen voor kleuren
 WHITE = (255, 255, 255)
@@ -15,37 +16,37 @@ BLACK = (0, 0, 0)
 GREEN = (0, 175, 0)
 
 # laad afbeeldingen
-loadingscreen = pg.image.load(r"img/loadingscreen_16_9.jpg")
+loadingscreen = pygame.image.load(r"img/loadingscreen_16_9.jpg")
 
 
 # beginscherm loop
 beginscherm = True
 while beginscherm:
-    for event in pg.event.get():
+    for event in pygame.event.get():
         # sluit de game af als er op het kruisje wordt geklikt
-        if event.type == pg.QUIT:
-            pg.quit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
         # gaat naar volgende loop als er geklikt wordt
-        if event.type == pg.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             beginscherm = False
             break
     # plaats achtergrondafbeelding in scherm
     screen.blit(loadingscreen, (0, 0))
     
     # update het scherm
-    pg.display.update()
+    pygame.display.update()
 
 while True:
-    for event in pg.event.get():
+    for event in pygame.event.get():
         # sluit de game af als er op het kruisje wordt geklikt
-        if event.type == pg.QUIT:
-            pg.quit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
             sys.exit()
     # achtergrond is groen
     screen.fill(GREEN)
+    banen.baan()
+    pygame.display.update()
 
-    pg.display.update()
-
-pg.quit()
+pygame.quit()
 sys.exit()
